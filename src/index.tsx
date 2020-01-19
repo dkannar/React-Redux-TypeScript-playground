15c4118ae0d34dd1e9d13eb923814e0d47b91d2a
Daniel Kannar <danielkannar@icloud.com>
2020-01-19 21:30:04 +0100

    First commit to RRTS playground

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,97 @@
+// 1) Basic setup of a React App with TypeScript
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// interface AppProps {
+//   // ? makes the prop optional
+//   color?: string;
+// }
+
+// class App extends React.Component<AppProps> {
+//   render() {
+//     return <div>{this.props.color}</div>;
+//   }
+// }
+
+// ReactDOM.render(<App color="red" />, document.getElementById('root'));
+
+// 2)
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// interface AppProps {
+//   // ? makes the prop optional
+//   color?: string;
+// }
+
+// interface AppState {
+//   counter: number;
+// }
+
+// class App extends React.Component<AppProps, AppState> {
+//   //   state = { counter: 0 };
+//   constructor(props: AppProps) {
+//     super(props);
+
+//     this.state = { counter: 0 };
+//   }
+
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
+
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//         {this.state.counter}
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<App color="red" />, document.getElementById('root'));
+
+// 3)
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// interface AppProps {
+//   // ? makes the prop optional
+//   color?: string;
+// }
+
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>;
+// };
+
+// ReactDOM.render(<App color="red" />, document.getElementById('root'));
+
+// __________________________
+// Setting up Redux
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { App } from './components/App';
+import { reducers } from './reducers/index';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
